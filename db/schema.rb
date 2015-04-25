c150acbd3a8b2dd89e4ca2063dae2afc92f4c1bf
Ciaran <otoole.ciaran@gmail.com>
2015-04-25 00:23:42 +0000

    user accounts made

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150424110954) do
+ActiveRecord::Schema.define(version: 20150425001627) do
 
   create_table "dish_rating_comments", force: true do |t|
     t.text     "content",        null: false
@@ -109,12 +109,23 @@ ActiveRecord::Schema.define(version: 20150424110954) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "first_name",  null: false
-    t.string   "last_name",   null: false
-    t.string   "hashed_pass"
-    t.string   "email",       null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "provider"
+    t.string   "uid"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
